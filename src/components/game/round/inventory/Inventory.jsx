0cd47e0c18fb792a6e42c69ddcaea1849ccd56a1
Placeblock <felix.weglehner@gmx.de>
2024-05-08 17:11:47 +0200

    Inventory now scrollable

--- src/components/game/round/inventory/Inventory.jsx
+++ src/components/game/round/inventory/Inventory.jsx
@@ -12,11 +12,6 @@ export default function Inventory() {
     const {sendMessage} = useContext(WebsocketContext);
     const amount = cards.length;
 
-    function getTransform(i) {
-        const trans = calcTranslation(amount, i);
-        const rot = calcRotation(amount, i);
-        return "translateX("+trans+"px) rotate(" + rot + "deg)";
-    }
     function handlePlace(uuid) {
         sendMessage("place_card", {"uuid":uuid});
     }
@@ -24,23 +19,19 @@ export default function Inventory() {
     return (
         <div className={`inventory ${currentPlayer!=user?"inventory-disabled":""}`}>
             {cards.map((c, i) => (
-                <button className="inventory-card" 
-                    onClick={() => handlePlace(c.uuid)}
-                    key={c.uuid}
-                    style={{transform: getTransform(i)}}>
-                    <Card card={c}/>
-                </button>
+                <div className="inventory-card-container">
+                    <button className="inventory-card" 
+                        onClick={() => handlePlace(c.uuid)}
+                        key={c.uuid}
+                        style={{transform: "translateX(-50%) rotate(" + calcRotation(amount,i) + "deg)"}}>
+                        <Card card={c}/>
+                    </button>
+                </div>
             ))}
         </div>
     )
 }
 
-export function calcTranslation(amount, i) {
-    const totalwidth = window.innerWidth/3;
-    const betweencardsx = Math.min(totalwidth/amount, 30);
-    const absolutecardsx = i*betweencardsx;
-    return absolutecardsx-betweencardsx*amount/2;
-}
 export function calcRotation(amount, i) {
     const betweencardsangle = 60/amount;
     const absoluteindexangle = (i+0.5)*betweencardsangle;
